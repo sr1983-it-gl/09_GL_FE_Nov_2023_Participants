c0623e92ff76374ce11662cf76962c9a5a2bb56e
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-06-23 14:51:26 +0530

    FE Lab-02

--- 04-Lab-02/Quiz-App/quiz.js
+++ 04-Lab-02/Quiz-App/quiz.js
@@ -16,6 +16,8 @@ function QuestionAnswerOptions(questionObj, answerOptionsObj){
   this.answerOptionsObj = answerOptionsObj;
 }
 
+// Question-Answer [1]
+
 const question1 = new Question("Javascript supports", 1);
 
 const answer1Q1 = new Answer("Functions");
@@ -28,5 +30,68 @@ const qA1 = new QuestionAnswerOptions(
   [answer1Q1, answer2Q1, answer3Q1, answer4Q1]
 )
 
-// TODO
-// Complete for the remaining 4 question-AnswerOptions
+// Question-Answer [2]
+
+const question2 
+  = new Question("Which language can be used for styling web pages ?", 2);
+
+const answer1Q2 = new Answer("HTML");
+const answer2Q2 = new Answer("JQuery");
+const answer3Q2 = new Answer("CSS");
+const answer4Q2 = new Answer("XML");
+
+
+const qA2 = new QuestionAnswerOptions(
+  question2, 
+  [answer1Q2, answer2Q2, answer3Q2, answer4Q2]
+)
+
+
+// Question-Answer [3]
+
+const question3
+  = new Question("Which is not a Javascript Framework", 3);
+
+const answer1Q3 = new Answer("Python Script");
+const answer2Q3 = new Answer("JQuery");
+const answer3Q3 = new Answer("Django");
+const answer4Q3 = new Answer("Node JS");
+
+
+const qA3 = new QuestionAnswerOptions(
+  question3, 
+  [answer1Q3, answer2Q3, answer3Q3, answer4Q3]
+)
+
+// Question-Answer [4]
+
+const question4
+  = new Question("Which is used to connect to Database?", 4);
+
+const answer1Q4 = new Answer("PHP");
+const answer2Q4 = new Answer("HTML");
+const answer3Q4 = new Answer("JS");
+const answer4Q4 = new Answer("All");
+
+
+const qA4 = new QuestionAnswerOptions(
+  question4, 
+  [answer1Q4, answer2Q4, answer3Q4, answer4Q4]
+)
+
+
+// Question-Answer [5]
+
+const question5
+  = new Question("Javascript is a ?", 5);
+
+const answer1Q5 = new Answer("Language");
+const answer2Q5 = new Answer("Programming Language");
+const answer3Q5 = new Answer("Development");
+const answer4Q5 = new Answer("All");
+
+
+const qA5 = new QuestionAnswerOptions(
+  question5, 
+  [answer1Q5, answer2Q5, answer3Q5, answer4Q5]
+)
