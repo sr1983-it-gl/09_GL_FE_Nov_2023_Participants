f2fa2918945d2d0149378695a5fb44b807a16ac0
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-06-23 14:41:30 +0530

    FE - Lab02

---
+++ 04-Lab-02/Quiz-App/quiz.js
@@ -0,0 +1,30 @@
+
+function Question (questionText, questionNo) {
+
+  this.questionText = questionText;
+  this.questionNo = questionNo;
+}
+
+function Answer (answerText){
+
+  this.answerText = answerText;
+}
+
+function QuestionAnswerOptions(questionObj, answerOptionsObj){
+
+  this.questionObj = questionObj;
+  this.answerOptionsObj = answerOptionsObj;
+}
+
+const question1 = new Question("Javascript supports", 1);
+
+const answer1Q1 = new Answer("Functions");
+const answer2Q1 = new Answer("XHTML");
+const answer3Q1 = new Answer("CSS");
+const answer4Q1 = new Answer("HTML");
+
+const qA1 = new QuestionAnswerOptions(
+  question1, 
+  [answer1Q1, answer2Q1, answer3Q1, answer4Q1]
+)
+
