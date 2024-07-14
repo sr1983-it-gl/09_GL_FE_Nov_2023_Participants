38f4d70a112f967dba05d77dbc47c5adcb096389
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-07-14 16:15:57 +0530

    FE-Lab-03

--- 06-Lab-03/Weather-App/weather-app/weather-app.js
+++ 06-Lab-03/Weather-App/weather-app/weather-app.js
@@ -3,9 +3,24 @@ class WeatherApp {
 
   init(){
 
+    const searchTextfieldElement = document.querySelector(".search-box")
+
+    searchTextfieldElement.addEventListener(
+      "keypress", this.handleAPIInvocation)
+
     // Add keypress listener
     // event-handler implementation
       // Enter key
         // event.key == 'Enter'
   }
+
+  handleAPIInvocation(event){
+
+    if (event.key == "Enter"){
+
+      console.log("Enter key pressed")
+    }
+  }
 }
+
+export {WeatherApp}
