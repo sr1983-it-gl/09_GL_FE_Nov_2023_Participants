ba248116c24027a4c247ae435be9970b348ab2b4
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-08-11 18:27:27 +0530

    FE-Lab-04

---
+++ 08-Lab-04/expense-splitter-app/client/src/components/ExpenseItemsView.js
@@ -0,0 +1,26 @@
+
+
+// Reference Code
+
+// Sample Expenses
+  // Place them in array
+  // 3 expense-items
+// Change the Table Headers
+  // Serial No
+  // Expense Description
+  // Payee Name
+  // Date
+  // Price
+// Table Body
+  // The Content has to be dynamic
+  // array.map( ) to render the expense-item
+  // Input 
+    // -> ExpenseItem [Sample]
+  // Output
+    // 
+      //   <tr>
+      //   <td>2</td>
+      //   <td>Jacob</td>
+      //   <td>Thornton</td>
+      //   <td>@fat</td>
+      // </tr>    
--- 08-Lab-04/expense-splitter-app/client/src/components/ExpenseSplitterApp.js
+++ 08-Lab-04/expense-splitter-app/client/src/components/ExpenseSplitterApp.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react"
 
+import {getAllExpenseItems} from "../service/expense"
+
 const ExpenseSplitterApp = () => {
 
   // Expense-Items
@@ -7,13 +9,25 @@ const ExpenseSplitterApp = () => {
 
   useEffect( () => {
 
+    const getAllExpenseItemsInvoker = async () => {
+
+      const response = await getAllExpenseItems()
+      console.log(`Expense Items -> ${JSON.stringify(response)}`)
+  
+    }
+
+    getAllExpenseItemsInvoker()
+
     // Add the logic
     // Call the service layer method
     // Print the result in the console    
-  })
+  }, [])
 
   return (
     <div>
+
+{/* Include the ExpenseItemsView component */}
+      
       ExpenseSplitter App Component
     </div>
   )  
