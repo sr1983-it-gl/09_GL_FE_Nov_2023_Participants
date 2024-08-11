084c97be26f02d0e7137baaf710eef65f568d2f4
sr1983-it-gl <sr1983.it.gl@gmail.com>
2024-08-11 17:59:55 +0530

    1

---
+++ 08-Lab-04/expense-splitter-app/client/src/components/ExpenseSplitterApp.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react"
+
+const ExpenseSplitterApp = () => {
+
+  // Expense-Items
+  // useEffect
+
+  useEffect( () => {
+
+    // Add the logic
+    // Call the service layer method
+    // Print the result in the console    
+  })
+
+  return (
+    <div>
+      ExpenseSplitter App Component
+    </div>
+  )  
+
+}
+
+export {ExpenseSplitterApp}
